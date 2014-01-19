d20caad84f20818fcc946205fdcb7ceeab9c381e
microflick <jithinrajiv@live.com>
2014-01-19 23:41:40 +0530

    Adding pages

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: node_modules/.bin/hr.js build -d client; node bin/reportr.js run --mode=web
+worker: node bin/reportr.js run --mode=worker
---
+++ client/views/views.js
@@ -0,0 +1,14 @@
+define([
+    'views/models.list',
+    
+    'views/reports/line',
+    'views/reports/count',
+    'views/reports/lastvalue',
+    'views/reports/list',
+    'views/reports/map',
+
+    'views/reports',
+    'views/report'
+], function() {
+	return arguments;
+});
